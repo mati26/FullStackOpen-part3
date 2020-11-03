2d12c9cbe1b9eccabf700c9c0f92db7c2449584a
mati26 <mati_b@op.pl>
2020-11-03 22:38:47 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
+app.use(express.static('build'))
 morgan.token('request-body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 app.use(express.json());
