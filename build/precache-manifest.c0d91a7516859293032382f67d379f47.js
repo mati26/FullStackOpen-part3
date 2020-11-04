21dad96ab765b3190bcd8488d221b8bb3a1fbb26
mati26 <mati_b@op.pl>
2020-11-04 21:40:10 +0100

    part3 minified react frontend

---
+++ build/precache-manifest.c0d91a7516859293032382f67d379f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb579d93e21c217941524b03acd39018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c54dad1532f55ca0b64",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fde8ac861ef41d88c8d2",
+    "url": "/static/js/2.9288c642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9288c642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c54dad1532f55ca0b64",
+    "url": "/static/js/main.c22dbce7.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
